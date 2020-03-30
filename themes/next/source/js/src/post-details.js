f434f33fdba3c96ba73a547971ab155abfbb3973
ning <1260408088@qq.com>
2020-03-31 00:17:43 +0800

    删除代码折叠

--- themes/next/source/js/src/post-details.js
+++ themes/next/source/js/src/post-details.js
@@ -80,15 +80,6 @@ $(document).ready(function () {
 
 });
 
-$(document).ready(function(){
-    $(document).on('click', '.fold_hider', function(){
-        $('>.fold', this.parentNode).slideToggle();
-        $('>:first', this).toggleClass('open');
-    });
-    //默认情况下折叠
-    $("div.fold").css("display","none");
-});
-
 $(document).ready(function () {
   var html = $('html');
   var TAB_ANIMATE_DURATION = 200;
